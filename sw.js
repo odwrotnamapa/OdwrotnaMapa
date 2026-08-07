20425765989673411479669b1d7f85167afbd3e4
Odwrotna Mapa <odwrotnamapa@protonmail.com>
2026-08-07 19:57:57 +0200

    landmarki

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // wyszukiwania/trasowania - to wymagałoby dużo większego
 // projektu (limity pamięci, dobór obszaru do zapisania itd.).
 
-const CACHE_VERSION = "shell-v149-20260812";
+const CACHE_VERSION = "shell-v150-20260812";
 
 // Osobna, ograniczona pamięć podręczna na kafelki mapy (wektorowe
 // z openfreemap.org i satelitarne z ArcGIS). W przeciwieństwie do
