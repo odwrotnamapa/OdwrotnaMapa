e378356162e2427e2e459e844916139a39dd59a4
Odwrotna Mapa <odwrotnamapa@protonmail.com>
2026-08-08 19:49:30 +0200

    panel wyglądu mapy

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // wyszukiwania/trasowania - to wymagałoby dużo większego
 // projektu (limity pamięci, dobór obszaru do zapisania itd.).
 
-const CACHE_VERSION = "shell-v177-mobile-fix";
+const CACHE_VERSION = "shell-v178-subpanel-class-fix";
 
 // Osobna, ograniczona pamięć podręczna na kafelki mapy (wektorowe
 // z openfreemap.org i satelitarne z ArcGIS). W przeciwieństwie do
