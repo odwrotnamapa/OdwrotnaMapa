a8f89be76eb57a43dfbe48907aee0121d14bbfe0
Odwrotna Mapa <odwrotnamapa@protonmail.com>
2026-08-11 09:02:52 +0200

    opis zmian

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // wyszukiwania/trasowania - to wymagałoby dużo większego
 // projektu (limity pamięci, dobór obszaru do zapisania itd.).
 
-const CACHE_VERSION = "gowno2";
+const CACHE_VERSION = "gowno3";
 
 // Osobna, ograniczona pamięć podręczna na kafelki mapy (wektorowe
 // z openfreemap.org i satelitarne z ArcGIS). W przeciwieństwie do
